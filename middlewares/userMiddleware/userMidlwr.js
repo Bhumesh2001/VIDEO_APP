87eb0af6be2c473fc8614b0769836637b6af3473
bhumesh2001 <bhumesh21@navgurukul.org>
2024-09-02 23:46:54 +0530

    new code

--- middlewares/userMiddleware/userMidlwr.js
+++ middlewares/userMiddleware/userMidlwr.js
@@ -2,11 +2,17 @@ const jwt = require('jsonwebtoken');
 
 exports.userAuthentication = async (req, res, next) => {
     try {
-        const token = req.headers['authorization'].split(' ')[1];
-
-        if (!token) {
-            return res.status(401).json({ message: 'Unautorized, Please login...' });
+        const authHeader = req.headers['authorization'];
+        if (!authHeader) {
+            return res.status(401).json({ message: 'Unauthorized, please login.' });
         };
+
+        const tokenParts = authHeader.split(' ');
+        if (tokenParts[0] !== 'Bearer' || !tokenParts[1]) {
+            return res.status(401).json({ message: 'Malformed authorization header.' });
+        };
+
+        const token = tokenParts[1];
         const decoded = jwt.verify(token, process.env.USER_SECRET_KEY);
 
         req.user = decoded;
@@ -14,7 +20,11 @@ exports.userAuthentication = async (req, res, next) => {
     } catch (error) {
         if (error.name === 'TokenExpiredError') {
             return res.status(401).json({ message: 'Token has expired, please login again.' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: 'Token is not valid.' });
+        } else {
+            console.error(error);
+            return res.status(500).json({ message: 'Internal server error.' });
         };
-        return res.status(401).json({ message: 'Token is not valid' });
     };
 };
